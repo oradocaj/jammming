5b7cfd4afb4e6f2ec0f78f74ebdc898dd3540de9
Ognjen Radočaj <oradocaj@team.nova.hr>
2021-09-08 15:27:08 +0200

    Render each track in the tracks prop

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import Playlist from './Components/Playlist/Playlist';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state.SearchResults
+    super(props);
+
+    this.state.searchResults = [
+      {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
+      {name: 'name4', artist: 'artist4', album: 'album4', id: 4}
+    ]
   }
+  
   render() {
     return (
       <div>
@@ -17,7 +24,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
             <Playlist />
           </div>
         </div>
