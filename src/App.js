de45f8cbdc57d2c937837f49386fac30ffd5c38b
oradocaj <oradocaj@gmail.com>
2021-09-09 22:13:27 +0200

    addTrack function finished

--- src/App.js
+++ src/App.js
@@ -11,14 +11,33 @@ class App extends React.Component {
 
     this.state = {
       searchResults: [
-        {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-        {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
-        {name: 'name4', artist: 'artist4', album: 'album4', id: 4}
+        { name: 'name1', artist: 'artist1', album: 'album1', id: 1 }, 
+        { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }, 
+        { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
+      ],
+      playlistName: 'My Playlist',
+      playlistTracks: [
+        { name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4 }, 
+        { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5 }, 
+        { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6 }
       ]
-    }
+    };
+
+    this.addTrack = this.addTrack.bind(this);
   }
   
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => 
+      savedTrack.id === track.id)) {
+        return;
+    }
+
+    this.state.playlistTracks.push(track);
+
+    this.setState({ playlistTracks: this.state.playlistTracks })
+  }
+
+
   render() {
     return (
       <div>
@@ -26,8 +45,10 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults}
+                           onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+                      playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,11 +6,12 @@ import Track from '../Track/Track';
 class TrackList extends React.Component {
     render() {
         return (
-            <div className="TrackList">
+            <div className="TrackList">                
                 {
                     this.props.tracks.map(track => {
-                        return <Track track={track} 
-                                      key={track.id} />
+                        return <Track track={track}
+                                      key={track.id}
+                                      onAdd={this.props.onAdd} />
                     })
                 }
             </div>
