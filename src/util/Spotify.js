22848d960f6227e9844c2bcb54475bac7d250a86
oradocaj <oradocaj@gmail.com>
2021-09-30 20:16:57 +0200

    search function remade

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -25,7 +25,7 @@ const Spotify = {
         }
     },
 
-    async search(term) {
+    /*async search(term) {
         const accessToken = Spotify.getAccessToken();
         const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
@@ -44,6 +44,29 @@ const Spotify = {
                 uri: track.uri
             }));
         }
+    },*/
+
+    serch(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if (!jsonResponse) {
+                return [];
+            } else {
+                return jsonResponse.tracks.items.map(track => ({
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }));
+            }
+        })        
     },
 
     async savePlaylist(name, trackUris) {
